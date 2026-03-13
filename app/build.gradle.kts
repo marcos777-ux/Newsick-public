5e2aecf1f04cd32f5413d06429da644c51fbf219
makro17 <marcosqh17@gmail.com>
2026-03-13 15:37:11 +0100

    3.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 3
-        versionName = "3.3.5"
+        versionName = "3.3.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
